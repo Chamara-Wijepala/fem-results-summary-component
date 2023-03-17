05dd8f0c6818d5a6916bf7a460e56f25bd0cb4c4
Chamara-Wijepala <mr.worf777@gmail.com>
2023-03-17 19:35:16 +0530

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    extend: {
+      colors: {
+        lightRed: "hsl(0, 100%, 67%)",
+        orangeyYellow: "hsl(39, 100%, 56%)",
+        greenTeal: "hsl(166, 100%, 37%)",
+        cobaltBlue: "hsl(234, 85%, 45%)",
+        lightSlateBlue: "hsl(252, 100%, 67%)",
+        lightRoyalBlue: "hsl(241, 81%, 54%)",
+        violetBlue: "hsl(256, 72%, 46%)",
+        persianBlue: "hsl(241, 72%, 46%)",
+        paleBlue: "hsl(221, 100%, 96%)",
+        lightLavender: "hsl(241, 100%, 89%)",
+        darkGrayBlue: "hsl(224, 30%, 27%)",
+      },
+    },
     fontFamily: {
-      hanken: ["Hanken Grotesk", "sans-serif"]
-    }
+      hanken: ["Hanken Grotesk", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
